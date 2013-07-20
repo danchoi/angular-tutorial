ec5cbedd8187a363323b97c63630a7ad5514f114
Daniel Choi <dhchoi@gmail.com>
2013-07-20 19:03:09 -0400

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
+function MyCtrl($scope) {
+  
+  $scope.items = [
+    {name: "Banana", rating: 3},
+    {name: "Vanilla", rating: 5},
+    {name: "Strawberry", rating: 7},
+    {name: "Rum Raisin", rating: 2}
+  ];
 
-console.log("hello");
+  $scope.addItem = function() {
+    console.log($scope.myForm);
+    if ($scope.myForm.$valid) {
+      $scope.items.push({name: $scope.newItem, rating: $scope.newRating});
+    } else {
+      console.log("form is invalid");
+    }
+  }
+
+}
