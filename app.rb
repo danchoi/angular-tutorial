bdb09bf026463c02ea49c563d793822bfbd0d38a
Daniel Choi <dhchoi@gmail.com>
2013-07-20 19:18:19 -0400

    updates

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'yaml'
+require 'json'
 
 set :public_dir, 'public' 
 
+get '/items.json' do
+  file = File.read('db.yml')
+  db = YAML::load(file)
+  db.to_json
+end
--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
 function MyCtrl($scope) {
   
-  $scope.items = [
-    {name: "Banana", rating: 3},
-    {name: "Vanilla", rating: 5},
-    {name: "Strawberry", rating: 7},
-    {name: "Rum Raisin", rating: 2}
-  ];
-
+  $scope.items = [];
   $scope.addItem = function() {
     console.log($scope.myForm);
     if ($scope.myForm.$valid) {
